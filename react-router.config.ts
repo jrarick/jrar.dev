a83f10accf8369e7cc70caf6df0fe7229a1aee84
Joshua Rarick <rarick.joshua.e@gmail.com>
2025-02-26 23:08:41 -0600

    bork

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,10 +1,6 @@
 import type { Config } from '@react-router/dev/config'
-import { getAllArticlesSlug } from './app/lib/blog.server'
 
 export default {
   ssr: true,
-  async prerender({ getStaticPaths }) {
-    const paths = getAllArticlesSlug().map((slug) => `/${slug}`)
-    return [...getStaticPaths(), ...paths]
-  },
+  prerender: true,
 } satisfies Config
