c49102fd757bd86bd3edbce492f6c89df46ff18a
Joshua Rarick <rarick.joshua.e@gmail.com>
2025-03-09 13:09:03 -0500

    bumped versions and added vercel preset and analytics

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,6 +1,8 @@
 import type { Config } from '@react-router/dev/config'
+import { vercelPreset } from '@vercel/react-router/vite'
 
 export default {
   ssr: true,
   prerender: true,
+  presets: [vercelPreset()]
 } satisfies Config
